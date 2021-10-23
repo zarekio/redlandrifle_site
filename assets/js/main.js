acc4e55a99e302a6d06540d3678f6ebb5f0b304f
JD <jd@zarek.io>
2021-10-22 20:47:03 -0500

    update links

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,8 +37,8 @@
 					// Images (in the format of 'url': 'alignment').
 						images: {
 							'images/bg01.png': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							// 'images/bg02.jpg': 'center',
+							// 'images/bg03.jpg': 'center'
 						},
 
 					// Delay.
